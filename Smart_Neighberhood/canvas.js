f976183738bebe290fb88287d03f3fd96544c09f
IdoLerer <idolerer222@gmail.com>
2019-12-16 14:42:52 +0200

    Animate person

--- Smart_Neighberhood/canvas.js
+++ Smart_Neighberhood/canvas.js
@@ -6,6 +6,8 @@ let isNextUpdated;
 let states;
 const numStates = 8;
 
+const animationTime = 96;
+
 const arrowC = "#ffffff";
 const arrowH = 10;
 const arrowW = 40;
@@ -25,16 +27,30 @@ const sidewalkC = "#efe5b0";
 const sidewalkH = 30;
 const streetC = "#3da744";
 const streetH = 125;
-const trashC = "#000000";
-const trashCanC = "#41484f";
+const trashC = "black";
+const trashCanC = "#c2c2c2";
 const roadH = canvasH - 2 * streetH;
 const truckH = (roadH*2)/3;
 const truckImageRatio = 564/516;
 const block = canvasW / numHouses;
 
-const initTopTruckX = - truckH * truckImageRatio;
+const personH = houseH * 1.5;
+const personImageRatio = 530 / 852;
+const personW = personH * personImageRatio;
+const personImg = new Image();
+personImg.src = "man.png";
+const initTopPersonX = - personW;
+const initTopPersonY = streetH - personH;
+const finalBottomPersonX = canvasW;
+const finalBottomPersonY = streetH + roadH - personH;
+let personX = initTopPersonX;
+let personY = initTopPersonY;
+
+// const initTopTruckX = - truckH * truckImageRatio;
+const initTopTruckX = - 3 * block / 4;
 const initTopTruckY = streetH - 20;
-const initBottomTruckX = canvasW;
+// const initBottomTruckX = canvasW;
+const initBottomTruckX = canvasW + block / 4;
 const initBottomTruckY = streetH + roadH / 2 - 20;
 let topTruckX = initTopTruckX;
 let topTruckY = initTopTruckY;
@@ -44,6 +60,7 @@ var truckImg = new Image();
 truckImg.src = "truck.png";
 var truckRevImg = new Image();
 truckRevImg.src = "truckRev.png";
+
 getNextState();
 
 function getNextState() {
@@ -71,9 +88,11 @@ function animate() {
   paintRoad();
   paintStreet(0, 0, canvasW, streetH, false);
   paintStreet(0, canvasH - streetH, canvasW, streetH, true);
+  paintStreetLamps(false);
   let animatingTop = animateTopTruck();
   let animatingBottom = animateBottomTruck();
-  if (!animatingTop && !animatingBottom && stateIndex < numStates) {
+  let animatingPerson = animatePerson();
+  if (!animatingPerson && !animatingTop && !animatingBottom && stateIndex < numStates) {
     if (isNextUpdated) {
       currentState = nextState;
       isNextUpdated = false;
@@ -85,7 +104,7 @@ function animate() {
   if (!isNextUpdated && !waitingResponse && stateIndex < numStates) {
     getNextState();
   }
-  paintStreetLamps();
+  paintStreetLamps(true);
   requestAnimationFrame(animate);
 }
 
@@ -93,7 +112,7 @@ function animateTopTruck() {
   let animating = false;
   ctx.drawImage(truckImg, topTruckX, topTruckY, truckH * truckImageRatio, truckH);
   if (topTruckX < block * currentState.truckTop + block / 4){
-    topTruckX += 3;
+    topTruckX += block / animationTime;
     animating = true;
   }
   if (currentState.topIsCleaning && topTruckY > initTopTruckY - 30){
@@ -111,7 +130,7 @@ function animateBottomTruck(){
   let animating = false;
   ctx.drawImage(truckRevImg, bottomTruckX, bottomTruckY, truckH * truckImageRatio, truckH);
   if (bottomTruckX > block * currentState.truckBottom + block / 4){
-    bottomTruckX -= 3;
+    bottomTruckX -= block / animationTime;
     animating = true;
   }
   if (currentState.bottomIsCleaning && bottomTruckY < initBottomTruckY + 30) {
@@ -120,6 +139,24 @@ function animateBottomTruck(){
   }
   if (!currentState.bottomIsCleaning && bottomTruckY > initBottomTruckY) {
     bottomTruckY -= 1;
+    animating = true;
+  }
+  return animating;
+}
+
+function animatePerson(){
+  let animating = false;
+  ctx.drawImage(personImg, personX, personY, personW, personH);
+  if (currentState.personTop && personX < (canvasW / 2) - (personW / 2)){
+    personX += (canvasW / 2 + personW / 2) / animationTime;
+    animating = true;
+  }
+  if (currentState.personCrossing && personY < finalBottomPersonY) {
+    personY += roadH / animationTime;
+    animating = true;
+  }
+  if (currentState.personBottom && personX < finalBottomPersonX) {
+    personX += (canvasW / 2 + personW / 2) / animationTime;
     animating = true;
   }
   return animating;
@@ -156,10 +193,14 @@ function paintStreet(left, top, width, height, isBottom){
   }
 }
 
-function paintStreetLamps() {
-  for (i = 0; i < numHouses / 2; i++) {
-        paintStreetLamp((2*i + 1) *block, streetH - houseH - sidewalkH / 2, houseW / 3, houseH, false);
-        paintStreetLamp((2*i + 1) *block, canvasH - streetH - houseH + sidewalkH / 2, houseW / 3, houseH, true);
+function paintStreetLamps(isBottom) {
+  for (i = 1; i < numHouses; i++) {
+    if (!isBottom) {
+      paintStreetLamp(i * block, streetH - houseH - sidewalkH / 2, houseW / 3, houseH, false);
+    }
+    else {
+      paintStreetLamp(i * block, canvasH - streetH - houseH + sidewalkH / 2, houseW / 3, houseH, true);
+    }
   }
 }
 
@@ -192,23 +233,27 @@ function paintStreetLamp(left, top, width, height, isBottom) {
 function paintHouse(index, isBottom, left, top, width, height) {
   ctx.fillStyle = houseC;
   ctx.fillRect(left, top, width, height);
-  ctx.fillStyle = "#832d25";
+  ctx.fillStyle = "#bd3d24";
   ctx.beginPath();
   ctx.moveTo(left, top);
   ctx.lineTo(left+width/2, top-30);
   ctx.lineTo(left+width, top);
   ctx.fill();
   ctx.fillStyle = trashCanC;
-  ctx.fillRect(left + width + 5, top + height / 2, width / 4, height / 2);
+  const trashCanW = width / 2;
+  const trashCanH = height / 2;
+  const trashH = height / 4;
+  const trashOffset = 12;
+  ctx.fillRect(left + width + trashOffset - 1, top + height  - trashCanH, trashCanW, trashCanH);
   let hasGarbage = isBottom ? currentState.garbageBottom[index] : currentState.garbageTop[index];
   if (hasGarbage) {
     ctx.fillStyle = trashC;
     ctx.beginPath();
-    ctx.moveTo(left + width + 6, top + height / 2);
-    ctx.lineTo(left + width + 8, top + height / 2 - 6);
-    ctx.lineTo(left + width + 12, top + height / 2 - 10);
-    ctx.lineTo(left + width + 3 + width / 4, top + height / 2 - 7);
-    ctx.lineTo(left + (5 * width) / 4 + 4, top + height / 2);
+    ctx.moveTo(left + width + trashOffset, top + trashCanH);
+    ctx.lineTo(left + width + trashOffset + trashCanW / 8, top + trashCanH - trashH *  2 / 3);
+    ctx.lineTo(left + width + trashOffset + trashCanW * 1 / 3, top + trashCanH - trashH);
+    ctx.lineTo(left + width + trashOffset + trashCanW * 2 / 3, top + trashCanH - trashH * 4 / 5);
+    ctx.lineTo(left + width + trashOffset - 2 + trashCanW, top + trashCanH);
     ctx.fill();
   }
   if (!isBottom) {
