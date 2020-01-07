d3ac4dce71853bed43c364885a21a47199a5baf3
IdoLerer <idolerer222@gmail.com>
2020-01-07 22:02:22 +0200

    More buttons

--- Smart_Neighberhood/controller.js
+++ Smart_Neighberhood/controller.js
@@ -27,6 +27,31 @@ function setMode(mode) {
   postToApi("{mode:" + mode + "}",callback);
 }
 
-function sendPedestrian(){
+function sendPedestrian() {
   postToApi("{pedestrian=true}");
 }
+
+function setNightDay(isNight) {
+  let time = isNight ? "NIGHT" : "DAY";
+  postToApi('{dayTime="' + time + '"}');
+}
+
+function setEnergySavingMode(isSaving) {
+  postToApi('{energyEfficiencyMode=' + isSaving + '}');
+}
+
+$(document).ready(function(){
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+      $("#" + this.responseText).addClass("active");
+      $("#" + this.responseText + " input").attr('checked', 'checked');
+      if (this.responseText === "AUTOMATIC") {
+        $("button").not('#dropdownMenuButton').attr("disabled", true);
+      }
+    }
+  };
+  xhttp.open("GET", "/api/current_mode", true);
+  xhttp.send();
+});
